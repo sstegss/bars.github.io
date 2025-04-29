166fde32f18b1eedc8c74039fece710419a62179
Amiryakinskiy <amirmukhamadiev05@yandex.ru>
2025-04-29 11:39:47 +0300

    final

--- src/pages/Deserialization.tsx
+++ src/pages/Deserialization.tsx
@@ -7,6 +7,8 @@ import CheckBox from "../components/CheckBox/CheckBox";
 import Plate from "../components/Plate/Plate";
 import "./Deserialization.css";
 
+import timemenegement from '../markdownText/Timemanagement/Timemanagement.md?raw';
+
 const Container = styled.div`
   display: inline-block;
   justify-content: center;
@@ -21,41 +23,41 @@ interface Props {
 }
 
 const Deserialization: FC<Props> = ({ back }) => {
-  const { name } = useParams(); // Получаем параметр из URL
-  const [file, setFile] = useState(""); // Состояние для данных
-  const [error, setError] = useState(""); // Состояние для ошибок
+  // const { name } = useParams(); // Получаем параметр из URL
+  // const [file, setFile] = useState(""); // Состояние для данных
+  // const [error, setError] = useState(""); // Состояние для ошибок
 
-  const API = "https://localhost:7006";
+  // const API = "https://localhost:7006";
 
-  useEffect(() => {
-    if (!name) {
-      setError("Название подтемы не указано");
-      return;
-    }
+  // useEffect(() => {
+  //   if (!name) {
+  //     setError("Название подтемы не указано");
+  //     return;
+  //   }
 
-    fetch(`${API}/api/SubThemes/${name}/content`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка: ${response.status}`);
-        }
-        return response.text(); // API возвращает текст (Markdown)
-      })
-      .then((data) => {
-        setFile(data); // Сохраняем данные
-      })
-      .catch((err) => {
-        setError(err.message);
-        console.error("Ошибка:", err);
-      });
-  }, [name]); // Зависимость от name — запрос обновится, если name изменится
+  //   fetch(`${API}/api/subthemes/${name}/content`, {mode:'no-cors'})
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`Ошибка: ${response.status}`);
+  //       }
+  //       return response.text(); 
+  //     })
+  //     .then((data) => {
+  //       setFile(data); 
+  //     })
+  //     .catch((err) => {
+  //       setError(err.message);
+  //       console.error("Ошибка:", err);
+  //     });
+  // }, [name]); // Зависимость от name — запрос обновится, если name изменится
 
   return (
     <>
       <Header back={back}></Header>
       <Container>
-        {error ? (
+        {/* {error ? (
           <p style={{ color: "red" }}>Ошибка: {error}</p>
-        ) : file ? (
+        ) : file ? ( */}
           <Markdown
             options={{
               overrides: {
@@ -74,11 +76,11 @@ const Deserialization: FC<Props> = ({ back }) => {
               },
             }}
           >
-            {file}
+            {timemenegement}
           </Markdown>
-        ) : (
+        {/* ) : (
           <p>Загрузка...</p>
-        )}
+        )} */}
       </Container>
     </>
   );
